e0b960f9cedd0b20f2c12b19fd5cc440915adaa2
flashcloud <flashcloud163@gmail.com>
2021-12-08 12:06:10 +0800

    Dependence comes from here

--- QrCodeGrailsPlugin.groovy
+++ QrCodeGrailsPlugin.groovy
@@ -5,7 +5,8 @@ class QrCodeGrailsPlugin {
     def title = "QRCode Plugin"
     def author = "Ly Tung"
     def authorEmail = "tyanhly@gmail.com"
-    def description = 'Allows your Grails application to create QRCode image which you can put your logo to this image'
+    def description = 'Allows your Grails application to create QRCode image which you can put your logo to this image. \n' +
+            'It depends on this library, kenglxn/QRGen:https://github.com/kenglxn/QRGen'
     def organization = [name: 'KiSS Concept', url: 'http://kiss-concept.com']
 
     def documentation = "http://grails.org/plugin/qr-code"
